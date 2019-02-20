178c741d6b0c03f3add39eda5a96a33c37dbf769
Vaughan Flood <Vaughan@Voodoo.local>
2019-02-20 14:22:55 +0000

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} =require('./models/user');
 
 var app = express();
+
+
 //app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,6 +31,23 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400),send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (! todo) {
+      return res.status(404).send();
+    }
+    return res.send({todo});
+  }).catch ((e) => {
+    res.status(400), send();
   });
 });
 
